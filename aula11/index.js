4d67dc5d8573efddc80e60be51c52036714e881c
Verônica Paranhos <veronicahp@gmail.com>
2022-10-21 15:10:41 -0300

    Atualizando aula

---
+++ aula11/index.js
@@ -0,0 +1,7 @@
+function adicionar() {
+    const nome = document.getElementById('nome').value
+    const idade = document.getElementById('idade').value 
+    document.getElementById('lista').innerHTML += 
+    `<p> Nome: ${nome} - idade: ${idade} anos</p>`
+  }
+  
